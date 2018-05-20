19bbf07af201c2ecd67f9f719d0fe5cb2ea34370
Arthur Gareginyan <arthurgareginyan@gmail.com>
2018-05-20 14:40:08 +0300

    Plugin updated to version 4.19

--- best-preloader.php
+++ best-preloader.php
@@ -5,7 +5,7 @@
  * Description: Easily add cross browser animated preloader to your website. It will be responsive and compatible with all major browsers. It will work with any theme!
  * Author: Space X-Chimp
  * Author URI: https://www.spacexchimp.com
- * Version: 4.18
+ * Version: 4.19
  * License: GPL3
  * Text Domain: best-preloader
  * Domain Path: /languages/
@@ -55,6 +55,7 @@ function spacexchimp_p007_define_constants( $constant_name, $value ) {
     if ( !defined( $constant_name ) )
         define( $constant_name, $value );
 }
+spacexchimp_p007_define_constants( 'FILE', __FILE__ );
 spacexchimp_p007_define_constants( 'DIR', dirname( plugin_basename( __FILE__ ) ) );
 spacexchimp_p007_define_constants( 'BASE', plugin_basename( __FILE__ ) );
 spacexchimp_p007_define_constants( 'URL', plugin_dir_url( __FILE__ ) );
