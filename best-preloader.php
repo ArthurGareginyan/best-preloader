120c9b2824263f24949fe7a25aeb06a2e68dc199
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-10-03 20:45:23 +0300

    Plugin updated to version 4.39

--- best-preloader.php
+++ best-preloader.php
@@ -5,7 +5,7 @@
  * Description: Easily and safely add a preloader to your WordPress website.
  * Author: Space X-Chimp
  * Author URI: https://www.spacexchimp.com
- * Version: 4.38
+ * Version: 4.39
  * License: GPL3
  * Text Domain: best-preloader
  * Domain Path: /languages/
